5067021f1921aee2181d74c8242845151325b32e
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-04-10 13:43:26 +0900

    Firebase Add

--- app/src/main/java/weather/fastcampus/firebase/MainActivity.kt
+++ app/src/main/java/weather/fastcampus/firebase/MainActivity.kt
@@ -2,11 +2,28 @@ package weather.fastcampus.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
     }
 }
