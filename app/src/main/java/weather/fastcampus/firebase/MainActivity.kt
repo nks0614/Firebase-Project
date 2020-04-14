c0e5a278d4dcf57e2afb072c5a8d57512c67bb63
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-04-14 22:38:40 +0900

    음....

--- app/src/main/java/weather/fastcampus/firebase/MainActivity.kt
+++ app/src/main/java/weather/fastcampus/firebase/MainActivity.kt
@@ -1,8 +1,13 @@
 package weather.fastcampus.firebase
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +29,24 @@ class MainActivity : AppCompatActivity() {
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
                         .setTheme(R.style.GreenTheme)
+                        .setLogo(R.drawable.danny_black_transparency)
                         .build(),
                 RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                Toast.makeText(this,"로그인 실패, 다시 로그인을 하십시오",Toast.LENGTH_LONG ).show()
+            }
+        }
     }
 }
